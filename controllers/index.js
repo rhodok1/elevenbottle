56e1a5ce535d17b0f9b2890a43f13a4dc0eb760e
Michael Sunarto <sunarto.michael@gmail.com>
2022-03-23 20:56:50 +0700

    setup app.js routes dan buat controllers dan views done (router masi bermasalah)

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const {Order, Product, User} = require('../models');
+class Controller {
+    static home(req, res) {
+        res.render("home")
+    }
+}
+
+module.exports = Controller
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const Controller = require("../controllers");
+const router = express.Router();
+
+router.get("/", Controller.home)
