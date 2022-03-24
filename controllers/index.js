c20d17c148ffb8d28ac7203fab2215e8d25746c5
Michael Sunarto <sunarto.michael@gmail.com>
2022-03-24 17:28:10 +0700

    format data biar rapih

--- controllers/index.js
+++ controllers/index.js
@@ -4,38 +4,40 @@ const bcrypt = require("bcryptjs");
 class Controller {
   static home(req, res) {
     Product.findAll({
-			exclude: ['createdAt', 'updatedAt', 'description', 'CategoryId']
-		})
-    .then((products) => {
-      res.render("home", { products });
+      exclude: ["createdAt", "updatedAt", "description", "CategoryId"],
     })
-    .catch((err) => {
-      res.send(err)
-    })
+      .then((products) => {
+        res.render("home", { products });
+      })
+      .catch((err) => {
+        res.send(err);
+      });
   }
 
-	static productDetails(req, res) {
-		const { productId } = req.params
-		Product.findByPk(+productId, {
-      include: [{
-				model: ProductDetail,
-				attributes: ['description']
-			},
-			{
-				model: Category,
-				attributes: ['name']
-			}],
-			attributes: {
-				exclude: ['createdAt', 'updatedAt']
-			}
+  static productDetails(req, res) {
+    const { productId } = req.params;
+    Product.findByPk(+productId, {
+      include: [
+        {
+          model: ProductDetail,
+          attributes: ["description"],
+        },
+        {
+          model: Category,
+          attributes: ["name"],
+        },
+      ],
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
     })
-			.then((product) => {
-				res.render('productdetails', { product })
-			})
-			.catch((err) => {
-				res.send(err)
-			})
-	}
+      .then((product) => {
+        res.render("productdetails", { product });
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  }
 
   static registerFormGet(req, res) {
     res.render("register");
@@ -63,8 +65,8 @@ class Controller {
   }
 
   static loginPageGet(req, res) {
-    const {error} = req.query
-    res.render("login", {error});
+    const { error } = req.query;
+    res.render("login", { error });
   }
 
   static loginPagePost(req, res) {
@@ -78,8 +80,8 @@ class Controller {
           if (isValidPassword) {
             req.session.user = {
               id: user.id,
-              role: user.role
-            }
+              role: user.role,
+            };
             if (user.role === "admin") {
               return res.redirect("/admin");
             } else {
@@ -111,48 +113,48 @@ class Controller {
   static user(req, res) {
     const { id } = req.params;
     Product.findAll({
-      include: [{
-				model: ProductDetail,
-				attributes: ['description', 'id']
-			},
-			{
-				model: Category,
-				attributes: ['name']
-			}],
-			attributes: {
-				exclude: ['createdAt', 'updatedAt']
-			}
+      include: [
+        {
+          model: ProductDetail,
+          attributes: ["description", "id"],
+        },
+        {
+          model: Category,
+          attributes: ["name"],
+        },
+      ],
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
     }).then((data) => {
       res.render("user", { data, id });
     });
   }
   static logout(req, res) {
     req.session.destroy(() => {
-      res.redirect("/")
-    })
+      res.redirect("/");
+    });
   }
 
-	static userBuyGet(req, res) {
-		const { userId, productId } = req.params
-		Product.findByPk(+productId, {
-			exclude: ['createdAt', 'updatedAt']
-		})
-			.then((product) => {
-				res.render('buy', { userId, product })
-			})
-			.catch((err) => {
+  static userBuyGet(req, res) {
+    const { userId, productId } = req.params;
+    Product.findByPk(+productId, {
+      exclude: ["createdAt", "updatedAt"],
+    })
+      .then((product) => {
+        res.render("buy", { userId, product });
+      })
+      .catch((err) => {
         if (err.name === "SequelizeValidationError") {
           const errors = err.errors.map((el) => el.message);
           res.send(errors);
         } else {
           res.send(err);
         }
-			})
-	}
+      });
+  }
 
-	static userBuyPost(req, res) {
-
-	}
+  static userBuyPost(req, res) {}
 }
 
 module.exports = Controller;
