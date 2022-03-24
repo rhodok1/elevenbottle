ccdc06cedb6794a889bfe34ae4a4db067006fa79
Jiwo Kristi <jiwokristi@gmail.com>
2022-03-24 12:34:14 +0700

    fitur udah dicek jalan semua. Perubahan: nama static method register sama login dari registerForm => registerFormGet, register => registerFormPost, login juga perubahannya sama, utk kepentingan readability indentasi juga diperbaiki. Lalu, static method registerFormPost bagian const newUser => createdAt dan updatedAt gw pindahin ke hooks karena permintaan pemakaian hooks di requirements pair project, juga ada penambahan validasi di catch error registerFormPost, bisa dicek

--- controllers/index.js
+++ controllers/index.js
@@ -5,46 +5,55 @@ class Controller {
     static home(req, res) {
         res.render("home")
     }
-    static registerForm(req, res) {
+
+    static registerFormGet(req, res) {
         res.render("register")
     }
-    static register(req, res) {
-        const {email, password, role} = req.body
+
+    static registerFormPost(req, res) {
+        const { email, password, role } = req.body
         const newUser = {
             email,
             password,
-            role,
-            createdAt: new Date(),
-            updatedAt: new Date(),
+            role
         }
         User.create(newUser)
-        .then(() => {
-            res.redirect("/login")
-        })
-        .catch((err) => {
-            res.send(err)
-        })
+            .then(() => {
+                res.redirect("/login")
+            })
+            .catch((err) => {
+                if (err.name === 'SequelizeValidationError') {
+                    const errors = err.errors.map(el => el.message)
+                    res.send(errors)
+                } else {
+                    res.send(err)
+                }
+            })
     }
-    static loginPage(req, res) {
+
+    static loginPageGet(req, res) {
         res.render("login")
     }
-    static login(req, res) {
-        const {email, password} = req.body
-        User.findOne({where: {email}})
-        .then(user => {
-            if(user) {
-                const isValidPassword = bcrypt.compareSync(password, user.password)
-                if (isValidPassword) {
-                    return res.redirect("/")
-                } else {
-                    const error = "invalid username or password"
-                    return res.redirect(`/login?error=${error}`)
+
+    static loginPagePost(req, res) {
+        const { email, password } = req.body
+        User.findOne({
+            where: { email }
+        })
+            .then(user => {
+                if (user) {
+                    const isValidPassword = bcrypt.compareSync(password, user.password)
+                    if (isValidPassword) {
+                        return res.redirect("/")
+                    } else {
+                        const error = "invalid username or password"
+                        return res.redirect(`/login?error=${error}`)
+                    }
                 }
-            }
-        })
-        .catch(err => {
-            res.send(err)
-        })
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const Controller = require("../controllers");
 
 router.get("/", Controller.home)
-router.get("/register", Controller.registerForm)
-router.post("/register", Controller.register)
-router.get("/login", Controller.loginPage)
-router.post("/login", Controller.login)
 
+router.get("/register", Controller.registerFormGet)
+
+router.post("/register", Controller.registerFormPost)
+
+router.get("/login", Controller.loginPageGet)
+
+router.post("/login", Controller.loginPagePost)
 
 module.exports = router
