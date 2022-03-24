864a6a572ad5e954599aef3b5a06725613d07e59
Jiwo Kristi <jiwokristi@gmail.com>
2022-03-24 15:19:36 +0700

    perbaiki indentasi doang, sekarang udah bener-bener rapi, langsung accept changes gw aja mic, ga ada codingan yg berubah kok, cuma indentasi aja

--- controllers/index.js
+++ controllers/index.js
@@ -2,73 +2,73 @@ const { Order, Product, User } = require('../models');
 const bcrypt = require('bcryptjs');
 
 class Controller {
-    static home(req, res) {
-        res.render("home")
-    }
+  static home(req, res) {
+    res.render("home")
+  }
 
-    static registerFormGet(req, res) {
-        res.render("register")
-    }
+  static registerFormGet(req, res) {
+    res.render("register")
+  }
 
-    static registerFormPost(req, res) {
-        const { email, password, role } = req.body
-        const newUser = {
-            email,
-            password,
-            role
+  static registerFormPost(req, res) {
+    const { email, password, role } = req.body
+    const newUser = {
+      email,
+      password,
+      role
+    }
+    User.create(newUser)
+      .then(() => {
+        res.redirect("/login")
+      })
+      .catch((err) => {
+        if (err.name === 'SequelizeValidationError') {
+          const errors = err.errors.map(el => el.message)
+          res.send(errors)
+        } else {
+          res.send(err)
         }
-        User.create(newUser)
-            .then(() => {
-                res.redirect("/login")
-            })
-            .catch((err) => {
-                if (err.name === 'SequelizeValidationError') {
-                    const errors = err.errors.map(el => el.message)
-                    res.send(errors)
-                } else {
-                    res.send(err)
-                }
-            })
-    }
+      })
+  }
 
-    static loginPageGet(req, res) {
-        res.render("login")
-    }
-  
-    static loginPagePost(req, res) {
-        const { email, password } = req.body
-        User.findOne({
-            where: { email }
-        })
-          .then(user => {
-              if(user) {
-                  const isValidPassword = bcrypt.compareSync(password, user.password)
-                  if (isValidPassword) {
-                      if (user.role === "admin") {
-                          return res.redirect(`/admin/${user.id}`)
-                      } else {
-                          return res.redirect(`/user/${user.id}`)
-                      }
-                 } else {
-                      const error = "invalid username or password"
-                      return res.redirect(`/login?error=${error}`)
-                  }
-             }
-         })
-          .catch(err => {
-              res.send(err)
-         })
-    }
-  
-    static admin(req, res) {
-        const {id} = req.params
-        console.log(req.params);
-    }
-  
-    static user(req, res) {
-        const {id} = req.params
-        console.log(req.params);
-    }
+  static loginPageGet(req, res) {
+    res.render("login")
+  }
+
+  static loginPagePost(req, res) {
+    const { email, password } = req.body
+    User.findOne({
+      where: { email }
+    })
+      .then(user => {
+        if (user) {
+          const isValidPassword = bcrypt.compareSync(password, user.password)
+          if (isValidPassword) {
+            if (user.role === "admin") {
+              return res.redirect(`/admin/${user.id}`)
+            } else {
+              return res.redirect(`/user/${user.id}`)
+            }
+          } else {
+            const error = "invalid username or password"
+            return res.redirect(`/login?error=${error}`)
+          }
+        }
+			})
+      .catch(err => {
+        res.send(err)
+     	})
+  }
+
+  static admin(req, res) {
+    const { id } = req.params
+    console.log(req.params);
+  }
+
+  static user(req, res) {
+    const { id } = req.params
+    console.log(req.params);
+  }
 }
 
 module.exports = Controller
