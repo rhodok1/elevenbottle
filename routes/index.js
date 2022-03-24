4514b86b31ae4cc55d63f0e24bfdc5cc5f197342
Michael Sunarto <sunarto.michael@gmail.com>
2022-03-24 19:53:58 +0700

    revisi error message pada session

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ const Controller = require("../controllers");
 
 router.get("/", Controller.home);
 
-router.get('/product/:productId', Controller.productDetails)
-
 router.get("/register", Controller.registerFormGet);
 
 router.post("/register", Controller.registerFormPost);
@@ -14,14 +12,25 @@ router.get("/login", Controller.loginPageGet);
 
 router.post("/login", Controller.loginPagePost);
 
+router.get("/logout", Controller.logout)
+
+router.use(function(req, res, next) {
+    if(req.session.user) {
+        next()
+    } else {
+        const error = "Please login first"
+        res.redirect(`login?error=${error}`)
+    }
+})
+
   const authenticatedUser = (req, res, next) => {
       if(req.session.user.role === "user") {
           next()
       } else {
         const error = "You dont have access to this page"
         res.redirect(`/login?error=${error}`)
-      }
-  }
+    }
+}
   const authenticatedAdmin = (req, res, next) => {
     if(req.session.user.role === "admin") {
         next()
@@ -30,7 +39,6 @@ router.post("/login", Controller.loginPagePost);
       res.redirect(`/login?error=${error}`)
     }
 }
-router.get("/logout", Controller.logout)
 
 router.get("/admin", authenticatedAdmin, Controller.admin);
 
@@ -45,9 +53,5 @@ router.post("/admin/editProduct/:productId", authenticatedAdmin, Controller.admi
 router.get("/admin/deleteProduct/:productId", authenticatedAdmin, Controller.admin);
 
 router.get("/user/:id", authenticatedUser, Controller.user);
-
-router.get("/order/:userId/buy/:productId", Controller.userBuyGet);
-
-router.post("/order/:userId/buy/:productId", Controller.userBuyPost);
 
 module.exports = router;
